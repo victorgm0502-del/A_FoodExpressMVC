c3cebd0d8b993bed81e79a7009caf90b53714ee8
Víctor <victorgm0502@gmail.com>
2025-11-17 17:29:01 +0100

    version 1.1, clase 17/11/25, hicimos builder(solo principio)

---
+++ src/main/java/es/daw/foodexpressmvc/config/AppConfig.java
@@ -0,0 +1,26 @@
+package es.daw.foodexpressmvc.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class AppConfig {
+
+    @Value("${api.base-url}")
+    private String apiUrl;
+
+    @Value("${api.auth-url}")
+    private String authUrl;
+
+
+    @Bean
+    public WebClient webClientApi(WebClient.Builder builder) {
+
+        return builder
+                .baseUrl(apiUrl)
+                .build();
+
+    }
+}
